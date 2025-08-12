2903abecf24597563ffa2f81b9049c8a05508489
Tynahan <nusilite@gmail.com>
2025-08-12 16:10:03 +0900

    ✨ Feat(neon): Add Assigning progress to student from book feature

--- src/neon.ts
+++ src/neon.ts
@@ -1,7 +1,36 @@
-import { PrismaClient, app_user } from "../generated/prisma"
+import { PrismaClient, app_user, progress } from "../generated/prisma"
 
 const prisma = new PrismaClient()
 
 export const prismaUserFindMany = async () => prisma.app_user.findMany()
 
-export const prismaUserCreate = async (user: app_user) => prisma.app_user.create({data: user})
+export const prismaUserCreate = async (user: app_user) => prisma.app_user.create({ data: user })
+
+export const prismaAssignProgressFromBook = async (studentId: number, bookTitle: string) => {
+    const student = await prisma.app_user.findUnique({ where: { id: studentId } })
+    if (!student) { throw new Error("NotFoundError") }
+
+    const resultFindMany = await prisma.question_group.findMany({
+        where: { step: { topic: { book: { title: bookTitle } } } },
+        select: {
+            // id: true, step: { select: { topic: { select: { book: { select: { title: true } } } } } }
+            id: true
+        }
+    });
+
+    const progressArray = resultFindMany.reduce((acc: progress[], questionGroup) => {
+        const progress = {} as progress
+        progress.app_user_id = studentId
+        progress.question_group_id = questionGroup.id
+        progress.completed = "NOT_STARTED"
+        progress.in_progress_status = "TODAY_WORK"
+        progress.do_need_to_ask = false
+        acc.push(progress)
+        return acc
+    }, [])
+
+    const resultCreateMany = await prisma.progress.createMany({ data: progressArray })
+    console.log({ resultCreateMany })
+    // const book = await 
+    return resultCreateMany
+}
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,36 @@
 import express from "express"
-import { addUser, checkHealth, getAllUsers } from "./reqres"
+import { app_user } from "../generated/prisma"
+import { prismaAssignProgressFromBook, prismaUserCreate, prismaUserFindMany } from "./neon"
 
 const router = express.Router()
 
-router.get("/checkhealth", checkHealth)
-router.get("/manage", getAllUsers)
-router.post("/manage", addUser)
+router.get("/checkhealth", async (req, res) => {
+    res.status(200).json({ message: "----- health good" })
+})
+
+router.get("/manage", async (req, res) => {
+    const result = await prismaUserFindMany()
+    res.status(200).json(result)
+})
+
+router.post("/manage", async (req, res) => {
+    console.log("---- here")
+    const user = req.body as app_user
+    const result = await prismaUserCreate(user)
+    res.status(201).json(result)
+})
+
+router.post("/progress/student/:studentId", async (req, res) => {
+    const studentIdString = req.params.studentId
+    const studentId = Number(studentIdString)
+    if (!studentId) { throw new Error("ValidationError") }
+
+    const bookTitle = req.body
+    if (!bookTitle) { throw new Error("ValidationError") }
+
+    const result = await prismaAssignProgressFromBook(studentId, bookTitle)
+
+    res.status(201).json(result)
+})
 
 export default router
