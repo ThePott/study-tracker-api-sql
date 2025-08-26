6776eb189f40f276c94cddbe1fb034a83464f747
Tynahan <nusilite@gmail.com>
2025-08-26 18:26:27 +0900

    🎨 Chore(format): Indent with prettier

--- src/routers/progressRouter.ts
+++ src/routers/progressRouter.ts
@@ -1,16 +1,23 @@
 import express from "express"
-import { prismaAssignProgressFromBook, prismaGetAllProgress, prismaPatchProgress } from "../neon"
+import {
+    prismaAssignProgressFromBook,
+    prismaGetAllProgress,
+    prismaPatchProgress,
+} from "../neon"
 
 const progressRouter = express.Router()
-
 
 progressRouter.post("/student/:studentId", async (req, res) => {
     const studentIdString = req.params.studentId
     const studentId = Number(studentIdString)
-    if (!studentId) { throw new Error("ValidationError") }
+    if (!studentId) {
+        throw new Error("ValidationError")
+    }
 
     const bookTitle = req.body
-    if (!bookTitle) { throw new Error("ValidationError") }
+    if (!bookTitle) {
+        throw new Error("ValidationError")
+    }
 
     const result = await prismaAssignProgressFromBook(studentId, bookTitle)
 
@@ -21,7 +28,7 @@ progressRouter.get("/student/:studentId", async (req, res) => {
     const studentIdString = req.params.studentId
     const studentId = Number(studentIdString)
     const result = await prismaGetAllProgress(studentId)
-
+    console.log("this")
     res.status(200).json(result)
 })
 
