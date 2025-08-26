63a5e65e8a883be2d10905724200c0a03533b162
Tynahan <nusilite@gmail.com>
2025-08-27 08:55:29 +0900

    ✨ Feat(api): Add get patch to review check router

--- src/routers/reviewCheckRouter.ts
+++ src/routers/reviewCheckRouter.ts
@@ -2,11 +2,31 @@ import express from "express"
 
 const reviewCheckRouter = express.Router()
 
+reviewCheckRouter.get("/student/:studentId", async (req, res) => {
+    try {
+        const studentIdString = req.params.studentId
+        const studentId = Number(studentIdString)
+        res.status(200).json({studentId})
+    } catch (error) {
+
+    }
+})
+
 reviewCheckRouter.post("/student/:studentId", async (req, res) => {
     try {
         const studentIdString = req.params.studentId
         const studentId = Number(studentIdString)
         res.status(201).json({studentId})
+    } catch (error) {
+
+    }
+})
+
+reviewCheckRouter.patch("/student/:studentId", async (req, res) => {
+    try {
+        const studentIdString = req.params.studentId
+        const studentId = Number(studentIdString)
+        res.status(200).json({studentId})
     } catch (error) {
 
     }
